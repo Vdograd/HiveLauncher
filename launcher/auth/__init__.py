481d02919537df9707835cab9f21eec685e3ff19
Vdograd <vashodov@gmail.com>
2026-03-27 17:44:20 +0500

    commit

--- launcher/auth/__init__.py
+++
@@ -1,5 +0,0 @@
-# from .auth_manager import AuthManager
-# from .auth_verify import AuthVerify
-# from .encryption import encryption_password
-
-# __all__ = ["AuthManager", "AuthVerify", "encryption_password"]
--- launcher/utils/__init__.py
+++
@@ -1,6 +0,0 @@
-# from .configurator import Configurator
-# from .helper import Helper
-# from .logger import Logger
-# from .font_manager import FontManager
-
-# __all__ = ['Configurator', 'Helper', 'Logger', 'FontManager']
