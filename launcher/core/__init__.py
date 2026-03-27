613fee825350b1df5b0d8008d80fdf9ca9bc6560
Vdograd <vashodov@gmail.com>
2026-03-27 17:51:18 +0500

    commit

--- launcher/core/__init__.py
+++
@@ -1,5 +0,0 @@
-# from .texture_manager import TextureSize1024,TextureSize64
-# from .version_manager import VersionManager
-# from .skin_cape_manager import LoadCape,LoadSkin,ClearCape,ClearSkin
-
-# __all__ = ["TextureSize1024", "TextureSize64", "VersionManager", "LoadCape", "LoadSkin", "ClearCape", "ClearSkin"]
--- launcher/ui/style.py
+++
@@ -1,7 +0,0 @@
-from ..utils.configurator import Configurator
-
-configurator = Configurator()
-def set_style(window, name):
-    with open(f"{configurator.theme_folder}\\{name}.css", "r", encoding="ansi") as file:
-        style = file.read()
-        window.setStyleSheet(style)
